065aec185ef09e7be495cfc8ea098c6ac5d94394
Elder Marco <eldermarco@gmail.com>
2012-12-15 11:03:53 -0200

    vim skel: sem descrições no arquivo de modelo
        Este trecho estava mais atrapalhando do que ajudando. Por enquanto,
        foi melhor remover ele.

--- home/.vim/skel/skel.py
+++ home/.vim/skel/skel.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
-#
-#       Descrição:
-#
-#           Autor: %AUTOR% <%EMAIL%>
-#          Criado: %DATA%
-#      Modificado: 
-#-------------------------------------------------------------------------------
+
 import os
 import sys
-
-
